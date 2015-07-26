4e7a9caff2b9a553473521217f4fe3134669f48d
Randy Nistler <rpnistler@yahoo.com>
2015-07-26 15:28:57 -0500

    sass and compass

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
 		  gutil = require('gulp-util'),
 		  coffee = require('gulp-coffee'),
 		  browserify = require('gulp-browserify'),
+		  compass = require('gulp-compass'),
 		  concat = require('gulp-concat')
 		  ;
 
@@ -11,7 +12,8 @@ var coffeeSources = ['components/coffee/tagline.coffee'],
 			  'components/scripts/pixgrid.js',
 			  'components/scripts/tagline.js',
 			  'components/scripts/template.js'
-		  ]
+		  ],
+		  sassSources = ['components/sass/style.scss']
 		  ;
 
 gulp.task('log', function () {
@@ -20,19 +22,29 @@ gulp.task('log', function () {
 
 gulp.task('coffee', function () {
 	gulp.src(coffeeSources)
-			  .pipe(coffee({bare: true})
-						 .on('error', gutil.log)
-						 )
+			  .pipe(coffee({bare: true}))
+			  .on('error', gutil.log)
 			  .pipe(gulp.dest('components/scripts'))
 			  ;
 });
 
 gulp.task('js', function () {
 	gulp.src(jsSources)
-			  .pipe(concat('script.js')
-						 .on('error', gutil.log)
-						 )
+			  .pipe(concat('script.js'))
+			  .on('error', gutil.log)
 			  .pipe(browserify())
 			  .pipe(gulp.dest('builds/development/js'))
+			  ;
+});
+
+gulp.task('compass', function () {
+	gulp.src(sassSources)
+			  .pipe(compass({
+				  sass: 'components/sass',
+				  image: 'builds/development/images',
+				  style: 'expanded'
+			  }))
+			  .on('error', gutil.log)
+			  .pipe(gulp.dest('builds/development/css'))
 			  ;
 });
